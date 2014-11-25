c98c74778dc9e5d1ee31550d3196c9877be3c0f7
Dennis Smit <d.smit@aol.nl>
2014-11-25 14:39:23 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+
+    // All configuration goes here
+    grunt.initConfig({
+
+        jekyll: {
+            build : {
+                dest: '_site'
+            }
+        },
+
+        sass: {
+            options: {
+                includePaths: ['_sass']
+            },
+            dist: {
+                files: {
+                    'css/app.css': '_sass/*.scss'
+                }
+            }
+        },
+
+        watch: {
+            sass: {
+                files: 'sass/*.scss',
+                tasks: ['sass']
+            },
+            jekyll: {
+                files: ['_layouts/*.html', '_includes/*.md', 'css/app.css'],
+                tasks: ['jekyll']
+            }
+        },
+
+        browser_sync: {
+            files: {
+                src : ['_site/css/*.css']
+            },
+            options: {
+                watchTask: true,
+                ghostMode: {
+                    clicks: true,
+                    scroll: true,
+                    links: true,
+                    forms: true
+                },
+                server: {
+                    baseDir: '_site'
+                }
+            }
+        }
+
+    });
+
+    // Load the plugins
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-jekyll');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    // Custom tasks
+    grunt.registerTask('build', ['sass', 'jekyll']);
+    grunt.registerTask('default', ['build', 'browser_sync', 'watch']);
+};
---
+++ _posts/25-11-14-site-with-jekyll-grunt-and-sass.md
@@ -0,0 +1,7 @@
+---
+layout: post
+title:  "Site built with Jekyll, Grunt and SASS!"
+date:   2014-11-25 14:52:49
+categories: jekyll update
+---
+This site is built with Jekyll, Grunt and Sass!
